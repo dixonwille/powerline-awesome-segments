e8bcc6de11452b96f3088a2d80619509f6183fbc
Will Dixon <dixonwille@hotmail.com>
2018-05-20 21:27:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name             = 'powerline_awesome_segments',
     description      = 'Powerline segments for the everyday developer',
-    version          = '0.1.0a1',
+    version          = '0.1.0a2',
     keywords         = 'powerline segment cwd',
     license          = 'GPLv3',
     author           = 'Will E. Dixon',
     author_email     = 'dixonwille@hotmail.com',
     url              = 'https://github.com/dixonwille/powerline-awesome-segments',
-    packages         = ['powerline_awesome_segments'],
+    packages         = ['powerline_awesome_segments.go','powerline_awesome_segments.cwdpath','powerline_awesome_segments.git'],
     install_requires = ['powerline-status>=2.6', 'pygit2>=0.26.4,<0.27.0'],
     classifiers      = [
         'Development Status :: 3 - Alpha',
